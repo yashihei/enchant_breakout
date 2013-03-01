8cf85e8dd05eda67dcea0798d26fa2b13768be99
Kobayashi Ryuhei <yashihei@gmail.com>
2013-03-01 21:37:15 +0900

    bar追加

--- main.js
+++ main.js
@@ -4,24 +4,11 @@
  */
 enchant();
 
-window.onload = function() {
-
-	var core = new Core(320, 320);
-	//core.preload('block.png');
-	//core.preload('ball.png');
-	core.preload('bar.png');
-	core.fps = 60;
-	core.onload = function() {
-		var bar = Bar(150, 300);
-	}
-	core.start();
-};
-
 Bar = Class.create(Sprite, {
     initialize: function (x, y) {
 		//スプライトのコンストラクタ
-        enchant.Sprite.call(this, 30, 10);
-		this.image = game.assets['ball.png'];
+        Sprite.call(this, 30, 10);
+		this.image = game.assets['bar.png'];
 		//座標
 		this.x = x;
 		this.y = y;
@@ -29,9 +16,23 @@ Bar = Class.create(Sprite, {
 		this.frame = 0;
 
 		//イベェェント
-		if (core.input.left) this.x -= 5;
-		if (core.input.right) this.x += 5;
+		if (game.input.left) this.x -= 5;
+		if (game.input.right) this.x += 5;
 		//ルートシーンに追加
-        core.rootScene.addChild(this);
+        game.rootScene.addChild(this);
 	}
 });
+
+window.onload = function() {
+
+	var game = new Game(320, 320);
+	//game.preload('block.png');
+	//game.preload('ball.png');
+	game.preload('bar.png');
+	game.fps = 60;
+	game.onload = function() {
+		var bar = new Bar(150, 150);
+	}
+	game.start();
+};
+
