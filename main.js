b3cdca44e2b458d8f5ad322ed6dfc40cb8e08372
Kobayashi Ryuhei <yashihei@gmail.com>
2013-03-06 20:57:06 +0900

    色々と修正9leap追加

--- main.js
+++ main.js
@@ -9,12 +9,10 @@ var game = null;
 
 //定数
 var BAR_SPEED = 5;
-var BALL_SPEED = 5;
 
 window.onload = function() {
 	game = new Game(200, 250);
-	game.preload('ball.png', 'bar.png', 'block.png');
-	game.preload('pon.wav');
+	game.preload('ball.png', 'bar.png', 'block.png', 'pon.wav');
 	game.fps = 60;
 	game.onload = function() {
 		var bar = new Bar(100, 200);
@@ -29,26 +27,30 @@ window.onload = function() {
 		this.addEventListener('enterframe', function() {
 			if (ball.intersect(bar)) {
 				ball.y = bar.y - ball.height;
-				ball.angle = 90 - ((ball.x+5 - (bar.x+20)) / 25) * 60;
+				ball.dy *= -1;
+				ball.dx = ((ball.x+ball.width/2 - (bar.x+bar.width/2)) / 25) * 3;
 				ball.se.play();
 			}
-			var chk = false;
 			for (var i=0; i < 32; i++) {
 				if (blocks[i].flag == 1) continue;
 				if (blocks[i].intersect(ball)) {
 					blocks[i].flag = 1;
-					game.rootScene.removeChild(blocks[i]);
-					if(chk == false) {
-						if (ball.y+ball.height < blocks[i].y+BALL_SPEED || blocks[i].y+blocks[i].height-BALL_SPEED < ball.y) {
-							ball.angle = -ball.angle;
-						} else if (ball.x+ball.width < blocks[i].x+BALL_SPEED || blocks[i].x+blocks[i].width-BALL_SPEED < ball.x) {
-							ball.angle = 180 - ball.angle;
-						}
-						blocks[i].se.play();
-						chk = true;
+					//ボールの移動量が4以上にならないのを条件として
+					if (ball.y+ball.height < blocks[i].y+4) {
+						ball.dy = (ball.dy < 0 ? ball.dy*-1 : ball.dy);
+					} else if (blocks[i].y+blocks[i].height-4 < ball.y) {
+						ball.dy = (ball.dy > 0 ? ball.dy*-1 : ball.dy);
+					} else if (ball.x+ball.width < blocks[i].x+4) {
+						ball.dx = (ball.dx > 0 ? ball.dx*-1 : ball.dx);
+					} else if (blocks[i].x+blocks[i].width-4 < ball.x) {
+						ball.dx = (ball.dx < 0 ? ball.dx*-1 : ball.dx);
 					}
+					blocks[i].se.play();
+					game.rootScene.removeChild(blocks[i]);
 				}
 			}
+			// if (blocks.length < 20) game.end();
+			console.log(blocks.length);
 		});
 	}
 	game.start();
@@ -56,13 +58,10 @@ window.onload = function() {
 
 Bar = Class.create(Sprite, {
     initialize: function (x, y) {
-		//スプライトのコンストラクタ
         Sprite.call(this, 40, 10);
 		this.image = game.assets['bar.png'];
-		//座標
 		this.x = x - this.width/2;
 		this.y = y;
-		//イベェェント
 		this.addEventListener('enterframe', function() {
 			if (game.input.left) this.x -= BAR_SPEED;
 			if (game.input.right) this.x += BAR_SPEED;
@@ -70,7 +69,6 @@ Bar = Class.create(Sprite, {
 			if (game.width - this.width < this.x) this.x = game.width - this.width;
 			if (this.x < 0) this.x = 0;
 		});
-		//ルートシーンに追加
         game.rootScene.addChild(this);
 	},
 });
@@ -82,31 +80,32 @@ Ball = Class.create(Sprite, {
 		this.se = game.assets['pon.wav'].clone();
 		this.x = x;
 		this.y = y;
-		this.speed = BALL_SPEED;
+		this.dx = 2;
+		this.dy = -3;
 		this.angle = 40;
 		this.addEventListener('enterframe', function() {
 			//移動量
-			this.x += this.speed * Math.cos(this.angle/180 * Math.PI);
-			this.y -= this.speed * Math.sin(this.angle/180 * Math.PI);
+			this.x += this.dx;
+			this.y -= this.dy;
 			//左端に来た時
 			if (this.x < 0) {
 				this.x = 0;
-				this.angle = 180 - this.angle;
+				this.dx *= -1;
 			}
 			//右端に来た時
 			if (this.x > game.width - this.width) {
 				this.x = game.width - this.width;
-				this.angle = 180 - this.angle;
+				this.dx *= -1;
 			}
 			//上端に来た時
 			if (this.y < 0) {
 				this.y = 0;
-				this.angle = -this.angle;
+				this.dy *= -1;
 			}
 			//下端に来た時
 			if (this.y > game.height - this.height) {
 				this.y = game.height - this.height;
-				this.angle = -this.angle;
+				this.dy *= -1;
 			}
 		});
 		game.rootScene.addChild(this);
